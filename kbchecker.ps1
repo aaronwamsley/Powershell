a7352111e79a9b596434cf21210a75764b4a4b97
aaronwamsley <31226463+aaronwamsley@users.noreply.github.com>
2019-04-16 10:38:40 -0600

    Update kbchecker.ps1

--- kbchecker.ps1
+++ kbchecker.ps1
@@ -2,6 +2,9 @@
 # @Author     Aaron Wamsley                                  #                 
 # @Date       5/15/18                                        #
 # @Purpose    Check for the presence of specific KBs on PCs  #
+#                                                            #
+# @Update     4/16/19                                        #
+#             updated logging to new format.                 #
 ##############################################################
 
 ##############################################################
@@ -13,23 +16,30 @@ $Computers = Get-Content "c:\scripts\powershell\kbchecker\computers.txt"
 $logdir = "c:\scripts\powershell\kbchecker\logs"
 # Enter KB to be checked here  
 $Patch = Read-Host 'Enter the KB number - eg: KB3011780 ' 
+$loggingLevel = $WARNING
 
 ##############################################################
 # Main Execution                                             #
 ##############################################################
 # Main Loop
+write-log $DEBUG "beginning main loop"
 foreach ($Computer in $Computers){
+    write-log $DEBUG "trying $Computer"
     TRY{
         $sess = New-PSSession $Computer
+        write-log $INFO "PSSESION to $Computer established"
         $kb = Invoke-Command -Session $sess -ScriptBlock{"get-hotfix -id $Patch -ErrorAction SilentlyContinue"}
         if($kb){  
-            Write-Log "$patch is installed on $Computer"  
+            Write-Log $INFO "$patch is installed on $Computer"  
         }else{        
-            Write-Log "$patch is not installed on $Computer" $true
+            Write-Log $ERROR "$patch is not installed on $Computer"
         }#end if/else for $kb
         Remove-PSSession $sess
+        write-log $INFO "terminating PSSESSION to $Computer"
     }CATCH{  
-        Write-Log "Error occurred processing $computer" $true
-        Write-Log "Error: $_.Exception.Message" $true
+        Write-Log $ERROR "Error occurred processing $computer" $true
+        Write-Log $ERROR "Error: $_.Exception.Message" $true
     } #end try/catch
+    write-log $DEBUG "finished with $Computer"
 }#end main loop
+write-log $DEBUG "ending main loop"
